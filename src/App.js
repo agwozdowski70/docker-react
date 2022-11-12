59202baec80bc9948284eb4a94287510bed9b71e
parallels <adam@gwozdowski@gmail.com>
2022-11-12 17:56:20 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AG!AG
+          New Feature
         </p>
         <a
           className="App-link"
